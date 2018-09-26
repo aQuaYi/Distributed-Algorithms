3b7652e9d1001f1ce1fab315a77b0a81ba7ef119
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-09-26 13:57:12 +0800

    添加了 receiveTime 数据结构

---
+++ Mutual-Exclusion/code/receivedTime.go
@@ -0,0 +1,80 @@
+package mutual
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type receivedTime struct {
+	trq *timeRecordQueue
+	trs []*timeRecord
+	rwm sync.RWMutex
+}
+
+func newReceivedTime(all, me int) *receivedTime {
+	trq := new(timeRecordQueue)
+	trs := make([]*timeRecord, all)
+	for i := range trs {
+		if i == me {
+			continue
+		}
+		trs[i] = &timeRecord{}
+		heap.Push(trq, trs[i])
+	}
+	return &receivedTime{
+		trq: trq,
+		trs: trs,
+	}
+}
+
+func (rt *receivedTime) update(id, time int) {
+	rt.rwm.Lock()
+	defer rt.rwm.Unlock()
+	rt.trq.update(rt.trs[id], time)
+}
+
+func (rt *receivedTime) min() int {
+	rt.rwm.RLock()
+	defer rt.rwm.RUnlock()
+	return (*rt.trq)[0].time
+}
+
+// timeRecord 是 priorityQueue 中的元素
+type timeRecord struct {
+	time  int
+	index int
+}
+
+type timeRecordQueue []*timeRecord
+
+func (trq timeRecordQueue) Len() int { return len(trq) }
+
+func (trq timeRecordQueue) Less(i, j int) bool {
+	return trq[i].time < trq[j].time
+}
+
+func (trq timeRecordQueue) Swap(i, j int) {
+	trq[i], trq[j] = trq[j], trq[i]
+	trq[i].index = i
+	trq[j].index = j
+}
+
+// Push 往 pq 中放 entry
+func (trq *timeRecordQueue) Push(x interface{}) {
+	temp := x.(*timeRecord)
+	temp.index = len(*trq)
+	*trq = append(*trq, temp)
+}
+
+// Pop 从 pq 中取出最优先的 entry
+func (trq *timeRecordQueue) Pop() interface{} {
+	temp := (*trq)[len(*trq)-1]
+	temp.index = -1 // for safety
+	*trq = (*trq)[0 : len(*trq)-1]
+	return temp
+}
+
+func (trq *timeRecordQueue) update(entry *timeRecord, priority int) {
+	entry.time = priority
+	heap.Fix(trq, entry.index)
+}
---
+++ Mutual-Exclusion/code/receivedTime_test.go
@@ -0,0 +1,31 @@
+package mutual
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_receivedTime(t *testing.T) {
+	ast := assert.New(t)
+	all, me := 10, 0
+	rt := newReceivedTime(all, me)
+	// 把所有的接受值调整到较大的值
+	for i := 1; i < all; i++ {
+		rt.update(i, all+1)
+	}
+	// 依次按照以最小值更新第 i 个时间值
+	for i := all - 1; i > me; i-- {
+		expected := i
+		rt.update(i, i)
+		actual := rt.min()
+		ast.Equal(expected, actual)
+	}
+}
+
+func Test_receivedTime_updateItselfWillPanic(t *testing.T) {
+	ast := assert.New(t)
+	all, me := 10, 0
+	rt := newReceivedTime(all, me)
+	ast.Panics(func() { rt.update(me, 1) })
+}
