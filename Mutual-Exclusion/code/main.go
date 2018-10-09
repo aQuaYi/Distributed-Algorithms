31f52ae533f3217261c3ea5d5f3500418ce54c3f
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-09 10:35:42 +0800

    开始复制文件

--- Mutual-Exclusion/code/main.go
+++ Mutual-Exclusion/code/main.go
@@ -2,20 +2,8 @@ package mutualExclusion
 
 import (
 	"fmt"
-	"log"
-	"math/rand"
 	"time"
-
-	"github.com/aQuaYi/observer"
 )
-
-var needDebug = false
-
-func init() {
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-	debugPrintf("程序开始运行")
-	rand.Seed(time.Now().UnixNano())
-}
 
 func main() {
 	beginTime := time.Now()
@@ -29,41 +17,4 @@ func main() {
 	}
 
 	fmt.Printf("一共测试了 %d 轮，全部通过。共耗时 %s 。\n", count, time.Now().Sub(beginTime))
-}
-
-func newRound(all, occupyTimesPerProcess int) {
-	rsc := newResource(all * occupyTimesPerProcess)
-
-	prop := observer.NewProperty(nil)
-
-	ps := make([]Process, all)
-	// 需要一口气同时生成，保证所有的 stream 都能从同样的位置开始观察
-	for i := range ps {
-		p := newProcess(all, i, rsc, prop)
-		ps[i] = p
-	}
-	debugPrintf("~~~ 已经成功创建了 %d 个 Process ~~~", all)
-
-	stream := prop.Observe()
-	go func() {
-		for {
-			msg := stream.Next().(*message)
-			debugPrintf(" ## %s", msg)
-		}
-	}()
-
-	for _, p := range ps {
-		go func(p Process, times int) {
-			i := 0
-			debugPrintf("%s 开始申请资源", p)
-			for i < times {
-				p.Request()
-				i++
-			}
-		}(p, occupyTimesPerProcess)
-	}
-
-	rsc.wait()
-
-	fmt.Println(rsc.report())
 }
--- Mutual-Exclusion/code/process_test.go
+++ Mutual-Exclusion/code/process_test.go
@@ -1,1 +1,44 @@
 package mutualExclusion
+
+import (
+	"fmt"
+
+	"github.com/aQuaYi/observer"
+)
+
+func newRound(all, occupyTimesPerProcess int) {
+	rsc := newResource(all * occupyTimesPerProcess)
+
+	prop := observer.NewProperty(nil)
+
+	ps := make([]Process, all)
+	// 需要一口气同时生成，保证所有的 stream 都能从同样的位置开始观察
+	for i := range ps {
+		p := newProcess(all, i, rsc, prop)
+		ps[i] = p
+	}
+	debugPrintf("~~~ 已经成功创建了 %d 个 Process ~~~", all)
+
+	stream := prop.Observe()
+	go func() {
+		for {
+			msg := stream.Next().(*message)
+			debugPrintf(" ## %s", msg)
+		}
+	}()
+
+	for _, p := range ps {
+		go func(p Process, times int) {
+			i := 0
+			debugPrintf("%s 开始申请资源", p)
+			for i < times {
+				p.Request()
+				i++
+			}
+		}(p, occupyTimesPerProcess)
+	}
+
+	rsc.wait()
+
+	fmt.Println(rsc.report())
+}
