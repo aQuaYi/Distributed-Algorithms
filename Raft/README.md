3ff763cf7c8c0128755baf1755129f2726fa4187
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-15 10:15:33 +0800

    移动了方法的位置

--- Raft/README.md
+++ Raft/README.md
@@ -2,3 +2,6 @@
 
 Diego Ongaro 和 John Ousterhout 认为 Paxos 难以理解， 于是在 [《In Search of an Understandable Consensus Algorithm (Extended Version)》](raft-extended.pdf) 中以可理解为目标，提出了一种新的共识算法——Raft。
 
+- [x] 基本算法
+- [ ] 配置更改
+- [ ] 日志压缩
--- Raft/code/raft_method.go
+++ Raft/code/raft_method.go
@@ -9,23 +9,6 @@ import (
 
 import "github.com/aQuaYi/Distributed-Algorithms/Raft/code/labgob"
 
-// GetState 可以获取 raft 对象的状态
-// return currentTerm and whether this server
-// believes it is the leader.
-func (rf *Raft) GetState() (int, bool) {
-	var term int
-	var isLeader bool
-	// Your code here (2A).
-
-	// 添加 RLock 是为了避免在 Lock 期间读取到数据
-	rf.rwmu.RLock()
-	term = rf.currentTerm
-	isLeader = rf.state == LEADER
-	rf.rwmu.RUnlock()
-
-	return term, isLeader
-}
-
 //
 // save Raft's persistent state to stable storage,
 // where it can later be retrieved after a crash and restart.
@@ -104,77 +87,6 @@ func (rf *Raft) readPersist(data []byte) {
 	}
 
 	debugPrintf("%s readPersisted!", rf)
-}
-
-// Start 给 server 发送命令
-// the service using Raft (e.g. a k/v server) wants to start
-// agreement on the next command to be appended to Raft's log. if this
-// server isn't the leader, returns false. otherwise start the
-// agreement and return immediately. there is no guarantee that this
-// command will ever be committed to the Raft log, since the leader
-// may fail or lose an election.
-//
-// the first return value is the index that the command will appear at
-// if it's ever committed. the second return value is the current
-// term. the third return value is true if this server believes it is
-// the leader.
-//
-//
-func (rf *Raft) Start(command interface{}) (index, term int, isLeader bool) {
-
-	index = -1
-	term = -1
-	isLeader = false
-
-	// Your code here (2B).
-	// if command received from client:
-	// append entry to local log, respond after entry applied to state machine
-	rf.rwmu.Lock()
-	defer rf.rwmu.Unlock()
-
-	if rf.state != LEADER {
-		return
-	}
-
-	// 修改结果值
-	index = len(rf.logs)
-	term = rf.currentTerm
-	isLeader = true
-
-	// 生成新的 entry
-	entry := &LogEntry{
-		LogIndex: index,
-		LogTerm:  term,
-		Command:  command,
-	}
-
-	// 修改 rf 的属性
-	rf.logs = append(rf.logs, *entry)
-	rf.nextIndex[rf.me] = len(rf.logs)
-	rf.matchIndex[rf.me] = len(rf.logs) - 1
-
-	debugPrintf("%s 添加了新的 entry:%v", rf, *entry)
-
-	return
-}
-
-// Kill is
-// the tester calls Kill() when a Raft instance won't
-// be needed again. you are not required to do anything
-// in Kill(), but it might be convenient to (for example)
-// turn off debug output from this instance.
-//
-//
-func (rf *Raft) Kill() {
-	// Your code here, if desired.
-	debugPrintf("S#%d Killing", rf.me)
-
-	// 关闭前，先去检查一遍 apply
-	rf.toCheckApplyChan <- struct{}{}
-
-	close(rf.shutdownChan)
-
-	rf.shutdownWG.Wait()
 }
 
 func (rf *Raft) resetElectionTimer() {
