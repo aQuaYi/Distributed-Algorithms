8413dd12a28b44a89a51788d8534fe20dad3059b
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-11-02 10:51:04 +0800

    pow 在看代码和文章的过程中，逐步弥补遗漏的知识点

--- PoW/README.md
+++ PoW/README.md
@@ -2,4 +2,11 @@
 
 本目录下的代码由 [Jeiwan](https://github.com/Jeiwan) 的 [blockchain_go](https://github.com/Jeiwan/blockchain_go) 重构而来。
 
-本目录下的代码着重于演示 Pow 算法，会弱化其余部分。
+TODO: 本目录下的代码着重于演示 Pow 算法，会弱化其余部分。
+
+## 哈希计算
+
+## math/big 库
+
+## sha256 库
+
--- PoW/code/transaction_input.go
+++ PoW/code/transaction_input.go
@@ -13,6 +13,5 @@ type TXInput struct {
 // UsesKey checks whether the address initiated the transaction
 func (in *TXInput) UsesKey(pubKeyHash []byte) bool {
 	lockingHash := HashPubKey(in.PubKey)
-
 	return bytes.Compare(lockingHash, pubKeyHash) == 0
 }
